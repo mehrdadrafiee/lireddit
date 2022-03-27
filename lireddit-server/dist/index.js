03a916eccf54c7fcf6b2d2787fa10223a9d03113
Mehrdad Rafiee <mehrdad.rafiee@verizondigitalmedia.com>
2022-03-27 01:44:36 -0700

    Initial commit

---
+++ lireddit-server/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const Post_1 = require("./entities/Post");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const main = async () => {
+    const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
+    await orm.getMigrator().up();
+    const posts = await orm.em.find(Post_1.Post, {});
+    console.log(posts);
+};
+main().catch((error) => {
+    console.log(error);
+});
+//# sourceMappingURL=index.js.map
---
+++ lireddit-server/src/mikro-orm.config.ts
@@ -0,0 +1,15 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from 'path';
+
+export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    glob: '!(*.d).{js,ts}',
+  },
+  entities: [Post],
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
